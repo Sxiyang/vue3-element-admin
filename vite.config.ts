492440b95ee05b3e4ae5c3b2997e9361fb39e06a
桑航伟 <1076058014@qq.com>
2023-06-29 15:50:42 +0800

    feat:src 路径别名配置,element自导入入

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,66 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+// 自动导入配置
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// element 按需引入
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 // https://vitejs.dev/config/
-export default defineConfig({
-  // vite 代理
-  server: {
-    port: 9527,
-    host: true,
-  },
-  plugins: [vue()],
+
+import path from "path";
+const pathSrc = path.resolve(__dirname, "src");
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    // 路径别名
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
+    // vite 代理
+    server: {
+      port: 9527,
+      host: true,
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ["vue"],
+        eslintrc: {
+          enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
+          filepath: "./.eslintrc-auto-import.json", // 指定自动导入函数 eslint 规则的文件
+          globalsPropValue: true,
+        },
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({}),
+        ],
+        vueTemplate: true, // 是否在 vue 模板中自动导入
+        dts: path.resolve(pathSrc, "types", "auto-imports.d.ts"), // 指定自动导入函数TS类型声明文件路径
+      }),
+      Components({
+        resolvers: [
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ["ep"], // element-plus图标库，其他图标库 https://icon-sets.iconify.design/
+          }),
+        ],
+        dts: path.resolve(pathSrc, "types", "components.d.ts"), // 指定自动导入组件TS类型声明文件路径
+      }),
+      Icons({
+        // 自动安装图标库
+        autoInstall: true,
+      }),
+    ],
+  };
 });
